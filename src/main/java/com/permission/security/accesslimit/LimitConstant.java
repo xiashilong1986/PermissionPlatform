764cf74c279a3d7442eeb8be9aca7ebba080c550
xiashilong <xiashilong1986@163.com>
2020-07-24 12:58:02 +0800

    修改接口限流次数

--- src/main/java/com/permission/security/accesslimit/LimitConstant.java
+++ src/main/java/com/permission/security/accesslimit/LimitConstant.java
@@ -13,7 +13,7 @@ class LimitConstant {
     //限制毫秒数
     final static long DEFAULT_MILLISECOND = 60000L;
     //最大访问次数
-    final static int DEFAULT_MAX_COUNT = 20;//默认
-    final static int FIND_MAX_COUNT = 30; //查询
-    final static int MODIFY_MAX_COUNT = 10; //更新
+    final static int DEFAULT_MAX_COUNT = 40;//默认
+    final static int FIND_MAX_COUNT = 50; //查询
+    final static int MODIFY_MAX_COUNT = 30; //更新
 }
